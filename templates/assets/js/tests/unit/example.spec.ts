cf782aa6559ee18ead9f81dfb931412c5d727717
tetty0217 <tettaro_01291978@ezweb.ne.jp>
2019-02-28 18:31:26 +0900

    fix: e2e HomeForm.vue

--- templates/assets/js/tests/unit/example.spec.ts
+++ templates/assets/js/tests/unit/example.spec.ts
@@ -1,11 +1,11 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import HomeForm from '@/components/HomeForm.vue';
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
     // @ts-ignore
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(HomeForm, {
       propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
